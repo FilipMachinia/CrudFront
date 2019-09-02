c3133c50dd7ca6551c57ee552073f38b26094e1e
Filip <rubystick@gmail.com>
2019-09-02 10:49:07 +0100

    fm: missing unit tests

--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDialogModule, MatDividerModule, MatIconModule, MatProgressSpinnerModule, MatTableModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -8,7 +11,17 @@ describe('ProductsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ ProductsComponent ],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MatIconModule,
+        MatTableModule,
+        MatDividerModule,
+        MatProgressSpinnerModule,
+        HttpClientModule,
+        MatDialogModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +29,46 @@ describe('ProductsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.selectedProduct.valid).toBeFalsy();
+  });
+
+  it('name field validity', () => {
+    let name = component.selectedProduct.controls['name'];
+    expect(name.valid).toBeFalsy();
+
+    name.setValue("");
+    expect(name.hasError('required')).toBeTruthy();
+
+    name.setValue("A");
+    expect(name.errors.minlength).toBeTruthy();
+
+    name.setValue("John");
+    expect(name.errors.minlength).toBeTruthy();
+  });
+
+  it('plate field validity', () => {
+    let plate = component.selectedProduct.controls['plate'];
+    expect(plate.valid).toBeFalsy();
+
+    name.setValue("");
+    expect(plate.hasError('required')).toBeTruthy();
+
+    name.setValue("AA");
+    expect(plate.errors).toBeTruthy();
+
+    name.setValue("AA11");
+    expect(plate.errors).toBeTruthy();
+
+    name.setValue("AA11AAA");
+    expect(plate.errors).toBeFalsy();
   });
 });
